0d49112aee2d3b5114bfbce42a68c6697243e5fd
Miguel Angel <miguelangelpacompia@gmail.com>
2021-08-17 20:41:46 -0500

    finish app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from "./lazy";
 
-console.log('Happy hacking :)')
+
+const getImage = document.querySelector('#getImage')
+const URL = "https://randomfox.ca/floof/"
+
+const getImageApi = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    return data.image;
+  }
+
+const createNodeImage = async()=>{
+    const container = document.createElement('figure')
+    container.className = 'rounded-md bg-gray-300 w-auto'
+
+    const foximage = document.createElement('img')
+    foximage.className = 'mx-auto rounded'
+    foximage.width = '320'
+    foximage.dataset.src = await getImageApi()
+
+    container.appendChild(foximage)
+    app.append(container)
+    registerImage(foximage)
+}
+const app = document.querySelector('#app')
+getImage.addEventListener('click', createNodeImage)
+
+
+const clean = document.querySelector("button[type='reset']");
+clean.addEventListener("click", () => {
+  app.innerHTML = "";
+});
